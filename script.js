fe47a9d50c82f264a4d8a1a7814092723b8a750f
Kimberly West <kdenton993@gmail.com>
2022-03-14 15:24:12 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var createTask = function(taskText, taskDate, taskList) {
+    // create elements that make up a task item
+    var taskLi = $("<li>").addClass("list-group-item");
+    var taskSpan = $("<span>")
+      .addClass("badge badge-primary badge-pill")
+      .text(taskDate);
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(taskText);
+}  
