d652e1aec6f9949061e25467675bba91eff74a6a
Kimberly West <kdenton993@gmail.com>
2022-03-17 17:11:57 -0600

    button functionality and color change function on hours

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-var createTask = function(taskText, taskDate, taskList) {
-    // create elements that make up a task item
-    var taskLi = $("<li>").addClass("list-group-item");
-    var taskSpan = $("<span>")
-      .addClass("badge badge-primary badge-pill")
-      .text(taskDate);
-    var taskP = $("<p>")
-      .addClass("m-1")
-      .text(taskText);
-}  
 
 //moment script
 
 var m = moment();
 
 //date and time
-$("#currentDay").text(moment().format("LLLL"));
+$("#currentDay").text(moment().format(`LLLL`));
 
 $(document).ready(function() {
     colorChange();
-    WebGL2RenderingContext();
+    renderText();
+});
+
+function colorChange(){
+    var currentTime = moment().hours();
+    console.log("Current Time" + currentTime);
+
+//past present or future blocks
+$('.input').each(function () {
+    var scheduledTime = parseInt($(this).attr("id"));
+    console.log(scheduledTime);
+    if(currentTime > scheduledTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (currentTime <scheduledTime) {
+        $(this).removeClass("present");
+        $(this).removeClass("past")
+        $(this).addClass("future");
+    }
+    else {
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+    }
+})
+
+}
+// button functionality
+var eventText;
+var eventTime;
+
+$(".saveBtn").click(function() {
+    eventText = $(this).siblings(".input").val();
+    console.log(eventText);
+    eventTime = $(this).siblings(".hour").text();
+    console.log(eventTime);
+    localStorage.setItem(eventTime,JSON.stringify(eventText));
+
+
+colorChange();
+renderText();
+
+});
+
+$(".deletebtn").click(function() {
+    eventText = $(this).siblings(".input").val("");
+    eventText = $(this).siblings(".input").val();
+    eventTime = $(this).siblings(".hour").text();
+    localStorage.setItem(eventTime, JSON.stringify(eventText));
+
+    colorChange();
+    renderText();
 });
